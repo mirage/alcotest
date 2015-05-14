386bf1c75d24e6fa6df4718be5a5d511ef85e9c7
Thomas Gazagnaire <thomas@gazagnaire.org>
2015-05-14 14:51:33 +0100

    Add a simple example
    Path from @leowzukw

---
+++ examples/simple.ml
@@ -0,0 +1,53 @@
+(*
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+*)
+
+(* Build with
+ * ocamlbuild -pkg alcotest to_test.byte *)
+
+(* A module with functions to test *)
+module To_test = struct
+  let capit letter = Char.uppercase letter
+  let plus int_list = List.fold_left (fun a b -> a + b) 0 int_list
+end
+
+(* The tests *)
+let capit () =
+  OUnit.assert_equal 'A' (To_test.capit 'a')
+
+let plus () =
+  OUnit.assert_equal 7 (To_test.plus [1;1;2;3])
+
+let test_set = [
+  "Capitalize" , `Quick, capit;
+  "Add entries", `Slow , plus ;
+]
+
+(* Run it *)
+let () =
+  Alcotest.run "My first test" [
+    "test_set", test_set;
+  ]
