dc0cc52d3a35f043c0b028829f59340db1b45bc5
Jeremy Yallop <yallop@gmail.com>
2016-03-12 05:46:26 +0000

    Allow the exception raised by check_err to propagate through check_raises.

--- lib/alcotest.ml
+++ lib/alcotest.ml
@@ -632,10 +632,13 @@ let fail msg =
   show_line msg;
   check_err "Error %s." msg
 
+let collect_exception f =
+  try f (); None with e -> Some e
+
 let check_raises msg exn f =
   show_line msg;
-  try
-    f ();
+  match collect_exception f with
+    None -> 
     check_err "Fail %s: expecting %s, got nothing." msg (Printexc.to_string exn)
-  with e ->
+  | Some e ->
     ()
