bf1c20f09c9a14a4217ae54df073b4053bff9f9c
Anil Madhavapeddy <anil@recoil.org>
2016-10-25 11:57:41 +0100

    Add a `unit` testable (useful for functions with side-effects)

--- src/alcotest.ml
+++ src/alcotest.ml
@@ -543,6 +543,8 @@ let string = testable Fmt.string (=)
 
 let bool = testable Fmt.bool (=)
 
+let unit = testable (Fmt.unit "()") (=)
+
 let list e =
   let rec eq l1 l2 = match (l1, l2) with
     | (x::xs, y::ys) -> equal e x y && eq xs ys
