1c7dcf221d3c3f0915f2f7004737a60ff2eb1522
Rudi Grinberg <rudi.grinberg@gmail.com>
2017-06-09 03:26:07 -0400

    Switch to jbuilder

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -1,12 +1,2 @@
-#!/usr/bin/env ocaml
 #use "topfind"
-#require "topkg"
-open Topkg
-
-let () =
-  Pkg.describe "alcotest" @@ fun c ->
-  Ok [
-    Pkg.mllib "src/alcotest.mllib";
-    Pkg.test "examples/simple";
-    Pkg.test ~run:false "examples/bad";
-  ]
+#require "topkg-jbuilder.auto"
