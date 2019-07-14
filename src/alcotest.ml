0d2a29154d4ba99754de33fff1443b523cb05369
Your Name <you@example.com>
2019-07-14 21:17:24 +0200

    Test output directories: Create symlinks for each test suite, and a "latest" symlink.
    - The "latest" symlink points to the last test executed. This is useful to quickly navigate to whichever directory contains failing tests.
    
    - Additionally a symlink named after `t.name` (in turn named after `argv[0]` of the test executable) is created. This is useful when there are multiple test executables being compiled as part of a build chain.
    
    - After executing my test suite three times, my test dir now looks like:
      ```shell
      $ ls /home/user/ocaml/xi-rope/_build/_tests/ -la
      total 28
      drwx------ 5 user user 4096 Jul 14 05:48 .
      drwxr-xr-x 7 user user 4096 Jul 14 05:47 ..
      drwx------ 2 user user 4096 Jul 14 05:48 A3DC6B25-317A-4DA8-8BF8-FA2FE0FD5A3E
      drwx------ 2 user user 4096 Jul 14 05:48 CDF189EE-35C2-43E8-A6A0-D62F86BED1B3
      drwx------ 2 user user 4096 Jul 14 05:48 F4100A63-F959-47E5-889A-311F29BB2594
      lrwxrwxrwx 1 user user   75 Jul 14 05:48 latest -> /home/user/ocaml/xi-rope/_build/_tests/F4100A63-F959-47E5-889A-311F29BB2594
      lrwxrwxrwx 1 user user   75 Jul 14 05:48 tests.native -> /home/user/ocaml/xi-rope/_build/_tests/F4100A63-F959-47E5-889A-311F29BB2594
      ```
    
    fixes #147

--- src/alcotest.ml
+++ src/alcotest.ml
@@ -189,7 +189,19 @@ let mkdir_p path mode =
 
 let prepare t =
   let test_dir = output_dir t in
-  if not (Sys.file_exists test_dir) then mkdir_p test_dir 0o770
+  if not (Sys.file_exists test_dir) then begin
+    mkdir_p test_dir 0o770 ;
+    if Sys.unix || Sys.cygwin then begin
+      let this_exe = Filename.concat t.test_dir t.name
+      and latest = Filename.concat t.test_dir "latest" in
+      if Sys.file_exists this_exe then Sys.remove this_exe ;
+      if Sys.file_exists latest then Sys.remove latest ;
+      Unix.symlink ~to_dir:true test_dir this_exe ;
+      Unix.symlink ~to_dir:true test_dir latest ;
+    end
+  end else if not (Sys.is_directory test_dir) then
+    failwith (Fmt.strf "exists but is not a directory: %S" test_dir)
+
 
 let color c ppf fmt = Fmt.(styled c string) ppf fmt
 let red_s fmt = color `Red fmt
