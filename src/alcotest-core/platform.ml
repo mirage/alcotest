85367853a40cbe17cbfdc2e283d0baa4a82e4354
Hannes Mehnert <hannes@mehnert.org>
2020-04-23 13:56:26 +0000

    Documentation of Platform.S
    Co-Authored-By: linse <sschirme@gmail.com>

--- src/alcotest-core/platform.ml
+++ src/alcotest-core/platform.ml
@@ -8,12 +8,3 @@ module type S = sig
 end
 
 module type MAKER = functor (M : Monad.S) -> S with type return = Pp.run_result M.t
-
-module None (M : Monad.S) = struct
-  type return = Pp.run_result M.t
-
-  let time () = 0.
-  let prepare ~base:_ ~dir:_ ~name:_ = ()
-  let with_redirect _ fn = fn ()
-  let setup_std_outputs ?style_renderer:_ ?utf_8:_ () = ()
-end
--- src/alcotest/alcotest.mli
+++ src/alcotest/alcotest.mli
@@ -1,5 +1,7 @@
 include module type of Alcotest_core.Test
+(** @inline *)
 
 module Unix : Alcotest_core.Platform.MAKER
 
 include Alcotest_core.Cli.S with type return = unit
+(** @inline *)
