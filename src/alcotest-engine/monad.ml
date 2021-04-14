a8bcaf0aa4690f980be5d7bc40f3084e25ae2069
Craig Ferguson <me@craigfe.io>
2021-04-14 23:27:10 +0100

    De-duplicate Monad module types

--- src/alcotest-engine/monad.ml
+++ src/alcotest-engine/monad.ml
@@ -14,13 +14,7 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
 
-module type S = sig
-  type 'a t
-
-  val return : 'a -> 'a t
-  val bind : 'a t -> ('a -> 'b t) -> 'b t
-  val catch : (unit -> 'a t) -> (exn -> 'a t) -> 'a t
-end
+include Monad_intf
 
 module Identity = struct
   type 'a t = 'a
@@ -28,20 +22,6 @@ module Identity = struct
   let return x = x
   let bind x f = f x
   let catch f on_error = match f () with x -> x | exception ex -> on_error ex
-end
-
-module type EXTENDED = sig
-  include S
-
-  module Infix : sig
-    val ( >>= ) : 'a t -> ('a -> 'b t) -> 'b t
-    val ( >|= ) : 'a t -> ('a -> 'b) -> 'b t
-  end
-
-  module List : sig
-    val fold_map_s :
-      ('acc -> 'a -> ('acc * 'b) t) -> 'acc -> 'a list -> ('acc * 'b list) t
-  end
 end
 
 module Extend (M : S) = struct
--- src/alcotest-engine/monad_intf.ml
+++ src/alcotest-engine/monad_intf.ml
@@ -1,5 +1,5 @@
 (*
- * Copyright (c) 2019 Craig Ferguson <craig@tarides.com>
+ * Copyright (c) 2019-2021 Craig Ferguson <craig@tarides.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -14,5 +14,32 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
 
-include Monad_intf.Monad
-(** @inline *)
+module type S = sig
+  type 'a t
+
+  val return : 'a -> 'a t
+  val bind : 'a t -> ('a -> 'b t) -> 'b t
+  val catch : (unit -> 'a t) -> (exn -> 'a t) -> 'a t
+end
+
+module type EXTENDED = sig
+  include S
+
+  module Infix : sig
+    val ( >>= ) : 'a t -> ('a -> 'b t) -> 'b t
+    val ( >|= ) : 'a t -> ('a -> 'b) -> 'b t
+  end
+
+  module List : sig
+    val fold_map_s :
+      ('acc -> 'a -> ('acc * 'b) t) -> 'acc -> 'a list -> ('acc * 'b list) t
+  end
+end
+
+module type Monad = sig
+  module type S = S
+  module type EXTENDED = EXTENDED
+
+  module Identity : S with type 'a t = 'a
+  module Extend (M : S) : EXTENDED with type 'a t = 'a M.t
+end
