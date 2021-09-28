e155d715447ef232100e81fe6c7f929e38f73ad7
Hugo Heuzard <hugo.heuzard@nomadic-labs.com>
2021-09-28 14:00:46 +0200

    make some example run with js_of_ocaml

--- src/alcotest/runtime.js
+++ src/alcotest/runtime.js
@@ -23,3 +23,13 @@ function alcotest_after_test (vstdout, vstderr){
   caml_ml_channels[vstderr] = alcotest_saved_stderr;
   return 0;
 }
+
+
+//Provides: ocaml_alcotest_get_terminal_dimensions
+function ocaml_alcotest_get_terminal_dimensions(unit) {
+  var p = joo_global_object.process
+  if(p && p.stdout && p.stdout.columns && p.stdout.rows) {
+    return [0, p.stdout.rows, p.stdout.columns];
+  }
+  return 0;
+}
