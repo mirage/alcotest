0f3e033bd5a0d099b8fd9d86a4ac607d46af920f
Hugo Heuzard <hugo.heuzard@gmail.com>
2021-10-02 09:10:20 +0200

    fix terminal dimension in js

--- src/alcotest/runtime.js
+++ src/alcotest/runtime.js
@@ -28,7 +28,7 @@ function alcotest_after_test (vstdout, vstderr){
 function ocaml_alcotest_get_terminal_dimensions(unit) {
   var p = joo_global_object.process
   if(p && p.stdout && p.stdout.columns && p.stdout.rows) {
-    return [0, p.stdout.rows, p.stdout.columns];
+    return [0, [0, p.stdout.rows, p.stdout.columns]];
   }
   return 0;
 }
