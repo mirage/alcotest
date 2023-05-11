fd9dcd785b095b653c6c1d83617e5512a0f4ba92
joan thibault <joan.thibault@inria.fr>
2023-05-11 08:27:22 +0200

    added match_raises a generalized version of check_raises

--- src/alcotest-engine/test.ml
+++ src/alcotest-engine/test.ml
@@ -239,5 +239,18 @@ let check_raises ?here ?pos msg exn f =
             Fmt.pf ppf "%t%a %s: expecting %s, got %s." (pp_location ?here ?pos)
               Pp.tag `Fail msg (Printexc.to_string exn) (Printexc.to_string e))
 
+let match_raises ?here ?pos msg exnp f =
+	show_assert msg;
+  match collect_exception f with
+  | None ->
+      check_err (fun ppf () ->
+          Fmt.pf ppf "%t%a %s: got nothing."
+            (pp_location ?here ?pos) Pp.tag `Fail msg)
+  | Some e ->
+      if exnp e then
+        check_err (fun ppf () ->
+            Fmt.pf ppf "%t%a %s: got %s." (pp_location ?here ?pos)
+              Pp.tag `Fail msg (Printexc.to_string e))
+
 let skip () = raise Core.Skip
 let () = at_exit (Format.pp_print_flush Format.err_formatter)
