1ac9f8a83c7815ac31441879761daa761d92f4bc
Hugo Heuzard <hugo.heuzard@lambda-coins.com>
2024-04-15 17:23:47 +0200

    Adapt jsoo runtime to new jsoo version

--- src/alcotest/runtime.js
+++ src/alcotest/runtime.js
@@ -1,26 +1,23 @@
 //Provides: alcotest_saved_stdout
-var alcotest_saved_stdout 
+var alcotest_saved_stdout
 //Provides: alcotest_saved_stderr
 var alcotest_saved_stderr
 
 //Provides: alcotest_before_test
-//Requires: caml_global_data, caml_ml_channels
+//Requires: caml_ml_channel_redirect
 //Requires: alcotest_saved_stderr, alcotest_saved_stdout
 function alcotest_before_test (voutput, vstdout, vstderr){
-  alcotest_saved_stderr = caml_ml_channels[vstderr];
-  alcotest_saved_stdout = caml_ml_channels[vstdout];
-  var output = caml_ml_channels[voutput];
-  caml_ml_channels[vstdout] = output;
-  caml_ml_channels[vstderr] = output;
+  alcotest_saved_stderr = caml_ml_channel_redirect(vstderr,voutput);
+  alcotest_saved_stdout = caml_ml_channel_redirect(vstdout,voutput);
   return 0;
 }
 
 //Provides: alcotest_after_test
-//Requires: caml_global_data, caml_ml_channels
+//Requires: caml_ml_channel_restore
 //Requires: alcotest_saved_stderr, alcotest_saved_stdout
 function alcotest_after_test (vstdout, vstderr){
-  caml_ml_channels[vstdout] = alcotest_saved_stdout;
-  caml_ml_channels[vstderr] = alcotest_saved_stderr;
+  caml_ml_channel_restore(vstdout, alcotest_saved_stdout);
+  caml_ml_channel_restore(vstderr, alcotest_saved_stderr);
   return 0;
 }
 
